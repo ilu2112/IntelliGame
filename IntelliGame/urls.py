cb5e99e524190aaffa60d0fb892b5115bf1eea5f
ilu2112 <cines91@gmail.com>
2012-12-30 14:31:21 +0100

    Admin included

--- IntelliGame/urls.py
+++ IntelliGame/urls.py
@@ -1,21 +1,16 @@
 from django.conf.urls import patterns, include, url
-# import UserManagement.views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+
 
 urlpatterns = patterns('',
     url(r'^user/', include('UserManagement.urls')),
     url(r'^$', 'IntelliGame.views.home'),
-    # url(r'^user/register$', UserManagement.views.register)
-    # Examples:
-    # url(r'^$', 'IntelliGame.views.home', name='home'),
-    # url(r'^IntelliGame/', include('IntelliGame.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
