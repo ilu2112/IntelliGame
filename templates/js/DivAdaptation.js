03e98c333868efa6de4522a36db4dfafa1c3bcc8
ilu2112 <cines91@gmail.com>
2013-07-02 08:12:07 +0200

    IE Layout hotfix

--- templates/js/DivAdaptation.js
+++ templates/js/DivAdaptation.js
@@ -36,10 +36,9 @@ function resizeDivs() {
 function getDocHeight() {
     var D = document;
     return Math.max(
-        D.body.scrollHeight, D.documentElement.scrollHeight,
-        D.body.offsetHeight, D.documentElement.offsetHeight,
         D.body.clientHeight, D.documentElement.clientHeight
     );
 }
+
 
 $(window).resize(resizeDivs);
